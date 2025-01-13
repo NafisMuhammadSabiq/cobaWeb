b51a44605ae12703839563ab39f927e60d0a56e2
NafisMuhammadSabiq <nafisharist@gmail.com>
2025-01-13 12:51:13 +0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,21 +1,37 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/about.html',
+    '/produk.html',
+    '/kontak.html',
+    '/style.css',
+    '/app.js',
+    '/gambar/logo.jpg',
+    '/gambar/kanoman.jpg',
+    '/gambar/mandalagm.jpg',
+    '/gambar/petirarafah.jpg',
+    '/gambar/sasukejetli.jpg',
+    '/gambar/sugalialpha.jpg',
+    '/gambar/Rtamu.jpg'
+];
+
+// Install the service worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/manifest.json'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
+// Fetch resources from cache
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
